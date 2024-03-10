d3657dc1ba649ead4ecfe09cb94b2e4c7138ec83
Harshitcoding <135004228+Harshitcoding@users.noreply.github.com>
2024-03-10 10:49:49 +0530

    Update Cart.jsx

--- tailwind/src/components/Cart.jsx
+++ tailwind/src/components/Cart.jsx
@@ -25,7 +25,7 @@ function Cart() {
                     <b style={{ margin: '10px 0' }}>{item.name}</b><br />
                     <b><strong>{item.subject}</strong></b><br />
                     <b>{item.description}</b><br />
-                    <button className='bg-blue-500 text-white rounded-md px-4 py-2 text-base font-semibold transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"' onClick={() => handleRemoveFromCart(item.id)}><img src='https://lucide.dev/icons/trash-2'/></button><br /> <br />
+                    <button className='bg-blue-500 text-white rounded-md px-4 py-2 text-base font-semibold transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"' onClick={() => handleRemoveFromCart(item.id)}><img src='https://cdn2.iconfinder.com/data/icons/squircle-ui/32/Trash-64.png'/></button><br /> <br />
                     <button onClick={handelSubmit} className='bg-pink-300 text-white rounded-md px-5 py-2 text-base font-medium transition duration-600 ease-in-out cursor-pointer'>
                         Proceed to pay
                     </button>
